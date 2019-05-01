f8b9610afefedb5a5607b599ae9e02e51a70e925
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-05-01 13:16:50 -0300

    added raised button, info text and adjusted the layout

--- lib/scenes/home/home.dart
+++ lib/scenes/home/home.dart
@@ -20,51 +20,83 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person,
-            size: 150.0,
-            color: Theme.of(context).primaryColor,
-          ),
-          Theme(
-            data: ThemeData(
-              hintColor: Colors.black,
-              primaryColor: Colors.teal,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          vertical: 8.0,
+          horizontal: 8.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.person,
+              size: 150.0,
+              color: Theme.of(context).primaryColor,
             ),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Weight (kg)",
-                alignLabelWithHint: true,
+            Theme(
+              data: ThemeData(
+                hintColor: Colors.black,
+                primaryColor: Colors.teal,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Weight (kg)",
+                  alignLabelWithHint: true,
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            Theme(
+              data: ThemeData(
+                hintColor: Colors.black,
+                primaryColor: Colors.teal,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Height (cm)",
+                  alignLabelWithHint: true,
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text(
+                "Calculate",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              color: Colors.black,
+              padding: EdgeInsets.all(12.0),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              "Info",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal,
               ),
               textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 20.0,
-              ),
-            ),
-          ),
-          Theme(
-            data: ThemeData(
-              hintColor: Colors.black,
-              primaryColor: Colors.teal,
-            ),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Height (cm)",
-                alignLabelWithHint: true,
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 20.0,
-              ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
