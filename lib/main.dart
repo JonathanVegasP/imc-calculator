c4eaa28ba6e47ac1f630fffee8cd758ab2e6b172
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-05-01 13:02:51 -0300

    added text fields and adjusted the location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:imc_calculator/scenes/home/home.dart';
 
 class ImcCalculator extends StatelessWidget {
@@ -6,8 +7,13 @@ class ImcCalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "IMC Calculator",
-      color: Colors.green,
+      theme: ThemeData.dark(),
       home: Home(),
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      localeResolutionCallback: (locale,locales) => locale,
     );
   }
 }
--- lib/scenes/home/home.dart
+++ lib/scenes/home/home.dart
@@ -12,11 +12,57 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("IMC Calculator"),
         centerTitle: true,
-        backgroundColor: Colors.green,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
             onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person,
+            size: 150.0,
+            color: Theme.of(context).primaryColor,
+          ),
+          Theme(
+            data: ThemeData(
+              hintColor: Colors.black,
+              primaryColor: Colors.teal,
+            ),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Weight (kg)",
+                alignLabelWithHint: true,
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          Theme(
+            data: ThemeData(
+              hintColor: Colors.black,
+              primaryColor: Colors.teal,
+            ),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Height (cm)",
+                alignLabelWithHint: true,
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 20.0,
+              ),
+            ),
           ),
         ],
       ),
